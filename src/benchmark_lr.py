d6adfc6f87dc31820029640be54c5e7d0fd8f1e4
Alon Samuel <--global>
2021-05-27 11:42:25 +0300

    Created a notebook for benchmark

--- src/benchmark_lr.py
+++ src/benchmark_lr.py
@@ -7,7 +7,7 @@ from sklearn import metrics
 from src import data_handler
 import numpy as np
 
-
+# Relevant functions
 def predict_classification(data, regressor):
     documents_regression = regressor.predict(data)
     documents_classification = np.round(documents_regression)
@@ -15,9 +15,10 @@ def predict_classification(data, regressor):
 
 def check_results(data_classification, data_target):
     accuracy_res = accuracy_score(data_target, data_classification)
-    
     print(f"Accuracy of prediction is - {accuracy_res:.4f}")
 
+
+# Get data
 newsgroups_train, newsgroups_test = data_handler.get_20newsgroups_data()
 
 X_train, X_test = data_handler.vectorize_text(newsgroups_train.data, newsgroups_test.data)
